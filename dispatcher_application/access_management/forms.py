e5478c39859f71659993e66fc751928b27b3296b
MartinR255 <ropjakm@gmail.com>
2022-12-21 21:23:58 +0100

    Access add and delete functionality

--- dispatcher_application/access_management/forms.py
+++ dispatcher_application/access_management/forms.py
@@ -3,6 +3,9 @@ from django import forms
 from .models import GroupBranchAccess, Group
 from branch_management.models import Branch
 from django.core.exceptions import ValidationError
+from access_management.models import UserGroupAccess
+from user_management.models import MyUser
+from django.db.models import Q
  
 
 class GroupAddForm(forms.Form):
@@ -43,4 +46,36 @@ class GroupUpdateForm(GroupAddForm):
             raise ValidationError(('Group with name \"%(value)s\" already exists'), params={'value': group_name})
         return group_name
 
+
+
+class AddGroupAccessForm(forms.ModelForm):
+
+    class Meta:
+        model = UserGroupAccess
+        fields = ['user_id', 'group_id']
+
+
+    def get_user_without_access(self, g_id):
+        raw_access_table_id = UserGroupAccess.objects.filter(group_id=g_id).values_list('user_id')
+        access_users_id = {uid[-1] for uid in set(raw_access_table_id)}
+
+        diff_user_id = MyUser.objects.exclude(id__in=access_users_id).values_list('id', 'email')
+        user_list = [u_info for u_info in diff_user_id] 
+        choice_list = [['', '---------']]
+        choice_list.extend(user_list)
+        return choice_list
+
+
+    def __init__(self, group_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['group_id'].label = 'Group'
+
+        # setup branch dropdown values
+        group_obj = Group.objects.get(id=group_id)
+        self.fields['group_id'].choices = [[group_obj.id, group_obj.name]]
+        
+        # setup user dropdown values
+        self.fields['user_id'].label = 'User'
+        user_list = self.get_user_without_access(group_id)
+        self.fields['user_id'].choices = user_list
 
