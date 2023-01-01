133e61595cf9a6b71d356b40be5b6df4d5ca2013
MartinR255 <ropjakm@gmail.com>
2023-01-01 22:55:30 +0100

    Detail view feture ready

--- dispatcher_application/user_management/urls.py
+++ dispatcher_application/user_management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListAllUsersView, RegisterNewUserView, UpdateUserView, DeleteUserView
+from .views import ListAllUsersView, RegisterNewUserView, UpdateUserView, DeleteUserView, UserDetailView
 
 urlpatterns = [
     path('list/', ListAllUsersView.as_view(), name='user-list'),
     path('add/', RegisterNewUserView.as_view(), name='user-add'),
     path('update/<int:pk>/', UpdateUserView.as_view(), name='user-update'),
-    path('delete/<int:pk>', DeleteUserView.as_view(), name='user-delete')
+    path('delete/<int:pk>', DeleteUserView.as_view(), name='user-delete'),
+    path('detail/<int:pk>', UserDetailView.as_view(), name='user-detail')
 ]
